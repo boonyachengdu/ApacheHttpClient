2669860405b92ff6874248dbda8ca793ff541b97
Allen Peng <boonya@sina.com>
2016-11-15 11:48:42 +0800

    Commit new codes!

--- src/main/java/com/boonya/client/SimpleHTTPClient.java
+++ src/main/java/com/boonya/client/SimpleHTTPClient.java
@@ -6,7 +6,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.UUID;
-
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
@@ -15,7 +14,6 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.ssl.SSLSocketFactory;
@@ -23,7 +21,6 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import com.alibaba.fastjson.JSONObject;
-
 /**
  * Apache HttpClient JSON参数请求方式
  * 
@@ -42,22 +39,36 @@ public class SimpleHTTPClient {
 		GET,POST
 	}
 	
-	// 接口地址
-	private String apiURL = "";
-
 	private HttpClient httpClient = null;
 
 	private Log logger = LogFactory.getLog(this.getClass());
 
 	private HttpPost methodPost = null;
 	
-	private HttpGet methodGet = null;
 
 	private long startTime = 0L;
 
 	private long endTime = 0L;
 
 	private int status = 0;
+	
+	private static class SingletonHttpClient {
+		/**
+		 * 单例对象实例
+		 */
+		static final SimpleHTTPClient INSTANCE = new SimpleHTTPClient();
+	}
+
+	private SimpleHTTPClient() {
+		httpClient = new DefaultHttpClient();
+	}
+	
+	/**
+	 * 单例实例
+	 */
+	public static SimpleHTTPClient getInstance() {
+		return SingletonHttpClient.INSTANCE;
+	}
 
 	/**
 	 * 设置忽略安全验证
@@ -68,7 +79,6 @@ public class SimpleHTTPClient {
 	 */
 	private void initHttps() {
 		// ==设置忽略访问SSL==
-		// 创建TrustManager
 		X509TrustManager xtm = new X509TrustManager() {
 			public void checkClientTrusted(X509Certificate[] chain,
 					String authType) throws CertificateException {
@@ -103,32 +113,6 @@ public class SimpleHTTPClient {
 		httpClient.getConnectionManager().getSchemeRegistry().register(sch);
 	}
 
-	/**
-	 * 接口地址
-	 * 
-	 * @param url
-	 */
-	public SimpleHTTPClient(String url) {
-		this.apiURL = url;
-		httpClient = new DefaultHttpClient();
-		setMethodPost();
-		// 设置忽略访问SSL
-		initHttps();
-	}
-
-	/**
-	 * 接口地址
-	 * 
-	 * @param url
-	 * @param uuid
-	 */
-	public SimpleHTTPClient(String url, String uuid) {
-		apiURL = url;
-		httpClient = new DefaultHttpClient();
-		setMethodPost(uuid);
-		// 设置忽略访问SSL
-		initHttps();
-	}
 	
 	/**
 	 * 设置Post方法
@@ -137,13 +121,13 @@ public class SimpleHTTPClient {
 	 * @Description: 
 	 * @throws
 	 */
-	private void setMethodPost(){
-		methodPost = new HttpPost(apiURL);
+	private void setMethodPost(String url){
+		methodPost = new HttpPost(url);
 		methodPost.setHeader("Accept", "application/json");
 		methodPost.setHeader("Accpet-Encoding", "gzip");
 		methodPost.setHeader("Content-Encoding", "UTF-8");
 		methodPost.setHeader("Content-Type", "application/json; charset=UTF-8");
-		methodPost.setHeader("Sequence",  UUID.randomUUID().toString());
+		methodPost.setHeader("Sequence", UUID.randomUUID().toString());
 	}
 	
 	/**
@@ -154,13 +138,13 @@ public class SimpleHTTPClient {
 	 * @param uuid
 	 * @throws
 	 */
-	private void setMethodPost(String uuid){
-		methodGet = new HttpGet(apiURL);
-		methodGet.setHeader("Accept", "application/json");
-		methodGet.setHeader("Accpet-Encoding", "gzip");
-		methodGet.setHeader("Content-Encoding", "UTF-8");
-		methodGet.setHeader("Content-Type", "application/json; charset=UTF-8");
-		methodGet.setHeader("Sequence", uuid);
+	private void setMethodPost(String url,String uuid){
+		methodPost = new HttpPost(url);
+		methodPost.setHeader("Accept", "application/json");
+		methodPost.setHeader("Accpet-Encoding", "gzip");
+		methodPost.setHeader("Content-Encoding", "UTF-8");
+		methodPost.setHeader("Content-Type", "application/json; charset=UTF-8");
+		methodPost.setHeader("Sequence", uuid);
 	}
 	
 	/**
@@ -169,7 +153,62 @@ public class SimpleHTTPClient {
 	 * @param parameters
 	 * @return
 	 */
-	public String post(String parameters) {
+	public String post(String url,String parameters) {
+		setMethodPost(url);
+		// 设置忽略访问SSL
+		initHttps();
+		String body = null;
+		logger.info("parameters:" + parameters);
+
+		if (methodPost != null & parameters != null
+				&& !"".equals(parameters.trim())) {
+			JSONObject jsonObject = JSONObject.parseObject(parameters);
+			logger.info("json:" + jsonObject.toString());
+			try {
+				StringEntity entity = new StringEntity(parameters, "UTF-8");
+				// 设置数据交付方式
+				entity.setContentType("application/json");
+				// 添加参数
+				methodPost.setEntity(entity);
+
+				startTime = System.currentTimeMillis();
+
+				// 设置编码
+				HttpResponse response = httpClient.execute(methodPost);
+				endTime = System.currentTimeMillis();
+				int statusCode = response.getStatusLine().getStatusCode();
+				logger.info("statusCode:" + statusCode);
+				logger.info("调用API 花费时间(单位：毫秒)：" + (endTime - startTime));
+				if (statusCode != HttpStatus.SC_OK) {
+					logger.error("Method failed:" + response.getStatusLine());
+					status = 1;
+				}
+				// Read the response body
+				body = EntityUtils.toString(response.getEntity());
+
+			} catch (IOException e) {
+				// 发生网络异常
+				logger.error("exception occurred!\n" + e.getMessage());
+				// 网络错误
+				status = 3;
+			} finally {
+				logger.info("调用接口状态：" + status);
+			}
+
+		}
+		return body;
+	}
+	
+	/**
+	 * 调用 API
+	 * 
+	 * @param parameters
+	 * @return
+	 */
+	public String post(String url,String parameters,String uuid) {
+		setMethodPost(url,uuid);
+		// 设置忽略访问SSL
+		initHttps();
 		String body = null;
 		logger.info("parameters:" + parameters);
 
--- src/test/java/com/boonya/client/SimpleHTTPClientTest.java
+++ src/test/java/com/boonya/client/SimpleHTTPClientTest.java
@@ -2,13 +2,18 @@ package com.boonya.client;
 
 import org.junit.Test;
 
+import com.alibaba.fastjson.JSONObject;
+
 public class SimpleHTTPClientTest {
 	
 	
 	@Test
 	public void testPost(){
-		String url="";
-		System.out.println(new SimpleHTTPClient(url));
+		String url="https://www.baidu.com/s?";
+		JSONObject json=new JSONObject();
+		json.put("keyword", "BBC");
+		json.put("wd", "BBC");
+		System.out.println(SimpleHTTPClient.getInstance().post(url, json.toJSONString())); 
 	}
 
 }
