96020eb2844de488ae957dc47358ba2cef65ae47
Allen Peng <boonya@sina.com>
2016-11-15 17:44:37 +0800

    Add class KeystoreHTTPSClient.java

---
+++ src/main/java/com/boonya/client/KeystoreHTTPSClient.java
@@ -0,0 +1,231 @@
+package com.boonya.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.net.ssl.SSLContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
+
+public class KeystoreHTTPSClient {
+	
+	private Log logger = LogFactory.getLog(this.getClass());
+	
+	 /** 
+     * 发送 get请求 
+     */  
+    public void get(String url) {  
+    	// 创建一个可以关闭的HttpClient
+        CloseableHttpClient httpclient = HttpClients.createDefault();  
+        try {  
+            // 创建httpget.    
+            HttpGet httpget = new HttpGet(url);  
+            logger.info("executing request " + httpget.getURI());  
+            // 执行get请求.    
+            CloseableHttpResponse response = httpclient.execute(httpget);  
+            try {  
+                // 获取响应实体    
+                HttpEntity entity = response.getEntity();  
+                logger.info("--------------------------------------");  
+                // 打印响应状态    
+                logger.info(response.getStatusLine());  
+                if (entity != null) {  
+                    // 打印响应内容长度    
+                	logger.info("Response content length: " + entity.getContentLength());  
+                    // 打印响应内容    
+                	logger.info("Response content: " + EntityUtils.toString(entity));  
+                }  
+                logger.info("------------------------------------");  
+            } finally {  
+                response.close();  
+            }  
+        } catch (ClientProtocolException e) {  
+            e.printStackTrace();  
+        } catch (ParseException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        } finally {  
+            // 关闭连接,释放资源    
+            try {  
+                httpclient.close();  
+            } catch (IOException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+    }  
+	
+	/** 
+     * HttpClient连接SSL 
+     */  
+    @SuppressWarnings("deprecation")
+	public void getSSL(String url,String keystorePath) {  
+    	// 创建一个可以关闭的HttpClient
+        CloseableHttpClient httpclient = null;  
+        try {  
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());  
+            // 加载keyStore keystorePath如,d:\\tomcat.keystore   
+            FileInputStream instream = new FileInputStream(new File(keystorePath));  
+            try {  
+                 
+                trustStore.load(instream, "123456".toCharArray());  
+            } catch (CertificateException e) {  
+                e.printStackTrace();  
+            } finally {  
+                try {  
+                    instream.close();  
+                } catch (Exception ignore) {  
+                }  
+            }  
+            // 相信自己的CA和所有自签名的证书  
+            SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(trustStore, new TrustSelfSignedStrategy()).build();  
+            // 只允许使用TLSv1、TLS、SSL协议  
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new String[] { "TLSv1","TLS","SSL" }, null,  
+                    SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);  
+            httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();  
+            // 创建http请求(get方式)  
+            HttpGet httpget = new HttpGet(url);  
+            logger.info("executing request" + httpget.getRequestLine());  
+            CloseableHttpResponse response = httpclient.execute(httpget);  
+            try {  
+                HttpEntity entity = response.getEntity();  
+                System.out.println(response.getStatusLine());  
+                if (entity != null) {  
+                	logger.info("--------------------------------------");  
+                	logger.info("Response content length: " + entity.getContentLength());  
+                	logger.info(EntityUtils.toString(entity));  
+                    EntityUtils.consume(entity);  
+                    logger.info("--------------------------------------");  
+                }  
+            } finally {  
+                response.close();  
+            }  
+        } catch (ParseException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        } catch (KeyManagementException e) {  
+            e.printStackTrace();  
+        } catch (NoSuchAlgorithmException e) {  
+            e.printStackTrace();  
+        } catch (KeyStoreException e) {  
+            e.printStackTrace();  
+        } finally {  
+            if (httpclient != null) {  
+                try {  
+                    httpclient.close();  
+                } catch (IOException e) {  
+                    e.printStackTrace();  
+                }  
+            }  
+        }  
+    }  
+  
+    /** 
+     * post方式提交表单（模拟用户登录请求） 
+     */  
+    public void post(String url,List<NameValuePair> parameters) {  
+    	// 创建一个可以关闭的HttpClient   
+        CloseableHttpClient httpclient = HttpClients.createDefault();  
+        // 创建httppost    
+        HttpPost httppost = new HttpPost(url);  
+       
+        UrlEncodedFormEntity uefEntity;  
+        try {  
+            uefEntity = new UrlEncodedFormEntity(parameters, "UTF-8");  
+            httppost.setEntity(uefEntity);  
+            logger.info("executing request " + httppost.getURI());  
+            CloseableHttpResponse response = httpclient.execute(httppost);  
+            try {  
+                HttpEntity entity = response.getEntity();  
+                if (entity != null) {  
+                	logger.info("--------------------------------------");  
+                	logger.info("Response content: " + EntityUtils.toString(entity, "UTF-8"));  
+                	logger.info("--------------------------------------");  
+                }  
+            } finally {  
+                response.close();  
+            }  
+        } catch (ClientProtocolException e) {  
+            e.printStackTrace();  
+        } catch (UnsupportedEncodingException e1) {  
+            e1.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        } finally {  
+            // 关闭连接,释放资源    
+            try {  
+                httpclient.close();  
+            } catch (IOException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+    }  
+  
+    /** 
+     * 发送 post请求访问本地应用并根据传递参数不同返回不同结果 
+     */  
+    public void post(String url) {  
+    	// 创建一个可以关闭的HttpClient   
+        CloseableHttpClient httpclient = HttpClients.createDefault();  
+        // 创建httppost    
+        HttpPost httppost = new HttpPost(url);  
+        // 创建参数队列    
+        List<NameValuePair> formparams = new ArrayList<NameValuePair>();  
+        formparams.add(new BasicNameValuePair("type", "house"));  
+        UrlEncodedFormEntity uefEntity;  
+        try {  
+            uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");  
+            httppost.setEntity(uefEntity);  
+            logger.info("executing request " + httppost.getURI());  
+            CloseableHttpResponse response = httpclient.execute(httppost);  
+            try {  
+                HttpEntity entity = response.getEntity();  
+                if (entity != null) {  
+                	logger.info("--------------------------------------");  
+                	logger.info("Response content: " + EntityUtils.toString(entity, "UTF-8"));  
+                	logger.info("--------------------------------------");  
+                }  
+            } finally {  
+                response.close();  
+            }  
+        } catch (ClientProtocolException e) {  
+            e.printStackTrace();  
+        } catch (UnsupportedEncodingException e1) {  
+            e1.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        } finally {  
+            // 关闭连接,释放资源    
+            try {  
+                httpclient.close();  
+            } catch (IOException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+    }  
+
+}
